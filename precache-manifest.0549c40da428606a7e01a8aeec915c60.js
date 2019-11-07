154c1e190ee220588fafc7542819546bdaf3485e
Anna Kazantseva <annetastara@gmail.com>
2019-11-07 08:55:17 +0200

    Updates

---
+++ precache-manifest.0549c40da428606a7e01a8aeec915c60.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be46dd8a139fc6416a3334a26de8f9aa",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "9b3a2cf29e57b7ec5b71",
+    "url": "/react_people-table/static/js/2.107816be.chunk.js"
+  },
+  {
+    "revision": "d1b20b51a8454efb5e7d",
+    "url": "/react_people-table/static/js/main.9dd5edd9.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_people-table/precache-manifest.3d8c6b1e7697442ca25d7e26bf334fd1.js"
+  "/react_people-table/precache-manifest.0549c40da428606a7e01a8aeec915c60.js"
 );
 
 self.addEventListener('message', (event) => {
